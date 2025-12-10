35c65a73af33a3ab4a0c357e9d9eb5951638fccb
himura2 <rangulov.05@mail.ru>
2025-12-10 17:39:58 +0500

    Поднял в докере бэк и бд + добавил автоматическое применение миграций при старте

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -102,6 +102,22 @@ public class Program
 
         var app = builder.Build();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<QuizDBContext>();
+                context.Database.Migrate(); 
+                Console.WriteLine("Database migrations applied successfully.");
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+        }
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
